c7bc58a601d68f70a8510e31379c7856d533818a
Andrey F <26743867+andyfrl@users.noreply.github.com>
2026-04-16 14:32:58 +0300

    add tests, add coverage

---
+++ __tests__/tags.test.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "vitest";
+import { Tag } from "../src/classes/Tag.js";
+
+test('test various tags', () => {
+	expect(new Tag("br").toString()).toBe("<br>");
+	expect(new Tag("br").toString()).toBe("<br>");
+	expect(new Tag("img", { src: "path/to/image" }).toString()).toBe("<img src=\"path/to/image\">");
+	expect(new Tag("input", { type: "submit", value: "Save" }).toString()).toBe("<input type=\"submit\" value=\"Save\">");
+	expect(new Tag("label",{}, "Email").toString()).toBe("<label>Email</label>");
+	expect(new Tag("div").toString()).toBe("<div></div>");
+});
--- eslint.config.ts
+++ eslint.config.ts
@@ -10,6 +10,6 @@ export default defineConfig([
   },
   tseslint.configs.recommended,
   {
-    ignores: ["dist/"],
+    ignores: ["dist/", "tsconfig.json"],
   },
 ]);
